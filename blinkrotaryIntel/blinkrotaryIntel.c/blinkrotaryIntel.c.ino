ec01b87dd2d0c2e3594f33b5edab2f8a096ef012
Phaninder Pasupula <pasupulaphani@gmail.com>
2016-05-15 09:58:42 +0100

    added all

---
+++ blinkrotaryIntel/blinkrotaryIntel.c/blinkrotaryIntel.c.ino
@@ -0,0 +1,53 @@
+int ledPin = 6;
+int potPin = A0;
+int notifier_pin   = 3;
+int js_subscriber_pin   = 6;
+
+FILE *fromarduino, *toarduino;
+
+int val = 0;
+int oldAngleVal = 0;
+int newAngleVal = 0;
+int blinkVal = 0;
+int counter = 0;
+int countMax = 10;
+
+void setup() {
+ // put your setup code here, to run once:
+// pinMode(ledPin, OUTPUT);
+
+ 
+  pinMode(notifier_pin, OUTPUT); //Notification pin     
+  pinMode(js_subscriber_pin, INPUT_PULLUP); //interrupt pin for reading message from JS    
+// Serial.begin(9600);
+}
+
+void loop() {
+ val = analogRead(potPin);
+ newAngleVal = map(val, 0, 1023, 0, 180);
+// Serial.write(newAngleVal);
+
+  if (newAngleVal > oldAngleVal + 2 || newAngleVal < oldAngleVal - 2) {
+    publishData(newAngleVal);
+    notifyWorld();  
+    oldAngleVal = newAngleVal;
+  }
+//
+// analogWrite(ledPin, val/4);
+}
+
+
+void publishData(int angle)
+{
+  fromarduino = fopen ("/arduino_notification_out.txt", "w+");
+  fprintf(fromarduino, "[%d]", angle);
+  fclose(fromarduino);  
+}
+
+//Nofity any body connected to this interrupt  (C++ program and NodeJS) program
+void notifyWorld()
+{
+    digitalWrite(notifier_pin, HIGH);
+    delay(200);
+    digitalWrite(notifier_pin, LOW);
+}
---
+++ ws-client/main.js
@@ -0,0 +1,77 @@
+/*jslint node:true, vars:true, bitwise:true, unparam:true */
+/*jshint unused:true */
+
+/*
+The Web Sockets Node.js sample application distributed within Intel® XDK IoT Edition under the IoT with Node.js Projects project creation option showcases how to use the socket.io NodeJS module to enable real time communication between clients and the development board via a web browser to toggle the state of the onboard LED.
+
+MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
+Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
+
+Steps for installing/updating MRAA & UPM Library on Intel IoT Platforms with IoTDevKit Linux* image
+Using a ssh client: 
+1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
+2. opkg update
+3. opkg upgrade
+
+OR
+In Intel XDK IoT Edition under the Develop Tab (for Internet of Things Embedded Application)
+Develop Tab
+1. Connect to board via the IoT Device Drop down (Add Manual Connection or pick device in list)
+2. Press the "Settings" button
+3. Click the "Update libraries on board" option
+
+Review README.md file for in-depth information about web sockets communication
+
+*/
+
+var mraa = require('mraa'); //require mraa
+console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the Intel XDK console
+//var myOnboardLed = new mraa.Gpio(3, false, true); //LED hooked up to digital pin (or built in pin on Galileo Gen1)
+var myOnboardLed = new mraa.Gpio(6); //LED hooked up to digital pin 13 (or built in pin on Intel Galileo Gen2 as well as Intel Edison)
+myOnboardLed.dir(mraa.DIR_OUT); //set the gpio direction to output
+var ledState = true; //Boolean to hold the state of Led
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+
+var connectedUsersArray = [];
+var userId;
+
+app.get('/', function(req, res) {
+    //Join all arguments together and normalize the resulting path.
+    res.sendFile(path.join(__dirname + '/client', 'index.html'));
+});
+
+//Allow use of files in client folder
+app.use(express.static(__dirname + '/client'));
+app.use('/client', express.static(__dirname + '/client'));
+
+
+
+/**********Read notification from arduino*************/
+var subscriber_pin = new mraa.Gpio(1);
+    subscriber_pin.dir(mraa.DIR_IN);
+    subscriber_pin.isr(mraa.EDGE_RISING, subscriberEvent); //Subscribe to interrupt notifications from Arduino
+    
+function subscriberEvent() {
+    var contents = fs.readFileSync('/arduino_notification_out.txt').toString();
+    console.log("Message from Arduino:" + contents);        
+}
+
+
+
+// websocket event handles
+var socket = require('socket.io-client')('http://172.16.40.213:3000');
+socket.on('connect', function(){});
+socket.on('event', function(data){});
+socket.on('disconnect', function(){});
+socket.compress(false).emit('an event', { some: 'data' });
+
+http.listen(3000, function(){
+    console.log('Web server Active listening on *:3000');
+});
+
